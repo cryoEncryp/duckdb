e98f3e6fafdd4391b6da34c5affbde55c2e20d74
Björn Bamberg <bjoern.bamberg@uni-tuebingen.de>
2024-05-02 10:38:38 +0200

    Switch to duckdb::shared_ptr

--- src/execution/physical_plan/plan_recursive_cte.cpp
+++ src/execution/physical_plan/plan_recursive_cte.cpp
@@ -81,7 +81,7 @@ unique_ptr<PhysicalOperator> PhysicalPlanGenerator::CreatePlan(LogicalRecursiveC
 		return std::move(cte);
 	} else {
 		// If the key variant has been used, a recurring table will be created.
-		auto recurring_table = std::make_shared<ColumnDataCollection>(context, op.types);
+		auto recurring_table = make_shared_ptr<ColumnDataCollection>(context, op.types);
 		recursive_cte_tables[op.recurring_index] = recurring_table;
 
 		auto right = CreatePlan(*op.children[1]);
